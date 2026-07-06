f5d80fd6addcdf170b22ca94c8e24da4a797edc7
nilparate <nilpatare7700@gmail.com>
2026-07-06 16:41:14 +0530

    AddTwo Number

---
+++ C++_Programming/Practice/Program2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+
+    cout<<"Enter First Number : ";
+    cin>>i;
+
+    cout<<"Enter Second Number : ";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition is :"<<k;
+
+    return 0;
+}
