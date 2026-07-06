db1bc07e07756c7011023ca463cb42be3a71eb5c
nilparate <nilpatare7700@gmail.com>
2026-07-06 16:42:13 +0530

    AddTwo Number

---
+++ C++_Programming/Practice/Program3.cpp
@@ -0,0 +1,30 @@
+/*
+    Algorithm
+
+    START
+        Accept First Number as No1
+        Accept Second Number as No2
+        Perform Addition of No1 & No2
+        Display The Result
+
+    STOP
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float i,j,k;
+
+    cout<<"Enter First Number : ";
+    cin>>i;
+
+    cout<<"Enter Second Number : ";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition is :"<<k;
+    return 0;
+}
