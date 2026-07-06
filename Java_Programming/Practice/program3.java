574b10feb8996058935879893888a49fbb24f1f8
nilparate <nilpatare7700@gmail.com>
2026-07-06 16:47:59 +0530

    Add progra3.java

---
+++ Java_Programming/Practice/program3.java
@@ -0,0 +1,36 @@
+/*
+    Algorithm
+
+    START
+        Accept First Number as No1
+        Accept Second Number as No2
+        Perform Addition of No1 & No2
+        Display The Result
+
+    STOP
+*/
+
+import java.util.Scanner;
+
+class program3
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1;
+        int iValue2;
+        int iResult;
+
+        System.out.println("Enter First Number : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Second Number : ");
+        iValue2 = sobj.nextInt();
+
+        iResult = iValue1 + iValue2;
+
+        System.out.println("Addition is : " + iResult);
+
+        sobj.close();
+    }
+}
