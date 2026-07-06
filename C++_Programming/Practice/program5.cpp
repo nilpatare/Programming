43ed5595032fcbad53223bd7d23ecdd12d51bb67
nilparate <nilpatare7700@gmail.com>
2026-07-06 16:45:18 +0530

    Add Program5.cpp

---
+++ C++_Programming/Practice/program5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float fValue1 = 0.0f;                   // To Store First Input
+    float fValue2 = 0.0f;                   // To Store Second Input
+    float fResult = 0.0f;                   // To Store the Result
+
+    cout<<"Enter First Number :";
+    cin>>fValue1;
+
+    cout<<"Enter Second Number : ";
+    cin>>fValue2;
+
+    fResult = fValue1 + fValue2;            // Perform the additon
+
+    return 0;
+}
+
