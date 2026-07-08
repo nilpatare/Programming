b6d449e3a0e8783a6f88470cfe1394cdbbafef5b
nilparate <nilpatare7700@gmail.com>
2026-07-08 15:04:19 +0530

    add filter and changes

---
+++ C_Programming/Practice/program29.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+     int iCnt = 0;
+
+    // filter
+     if(iNo < 0)
+    {
+        printf("Invalid Input\n");
+        return;
+    }
+
+     for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d : Jay Ganesh...\n",iCnt);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter The Frequency : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
