5fb47a6f60997ad0d5fc367fe24df4ae75d933a2
nilparate <nilpatare7700@gmail.com>
2026-07-09 23:34:33 +0530

    divisible type 3

---
+++ Java_Programming/Practice/program19.java
@@ -0,0 +1,43 @@
+// Type 3
+
+import java.util.Scanner;
+
+class NumberX 
+{
+    public static boolean CheckDivisible(int iNo)
+    {
+         if((iNo % 3 == 0) && (iNo % 5 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class program19
+{
+    public static void main(String A[]) 
+    {
+        int iValue = 0;
+        boolean bRet = false;
+        
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Number : ");
+        iValue = sobj.nextInt();
+
+        bRet = NumberX.CheckDivisible(iValue);
+
+        if(bRet == true)
+        {
+            System.out.println("Number is divisible by 3 & 5");
+        }
+        else
+        {
+            System.out.println("Number is not divisible by 3 & 5 ");
+        }
+    }
+}
