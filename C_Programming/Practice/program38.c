cddb5c53f5ea946df47f899c3ab9857e29d501f7
nilparate <nilpatare7700@gmail.com>
2026-07-11 01:23:52 +0530

    foor loop linear pattern

---
+++ C_Programming/Practice/program38.c
@@ -0,0 +1,27 @@
+// Input : 8
+// Output : 1   3   5   7 
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0 ;
+    for(iCnt = 1; iCnt <= iNo; iCnt = iCnt + 2)         // Linear pattern iCnt++ ----> iCnt = iCnt +1;  ----> iCnt +=2
+    {
+        printf("%d\t",iCnt);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0 ;
+    
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
