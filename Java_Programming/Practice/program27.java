d20f5cc2579c2af9ca1778a9944d6925d51a8ef2
nilparate <nilpatare7700@gmail.com>
2026-07-12 23:45:02 +0530

    using loop

---
+++ Java_Programming/Practice/program27.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class NumberX 
+{
+   public boolean CheckPrime(int iNo)
+   {
+        int iCnt = 0;
+        
+        for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                break;
+            }
+        }
+        if(iCnt >= (iNo/2)+1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class program27
+{
+    public static void main(String A[]) 
+    {
+        int iValue = 0;
+        boolean bRet = false;
+        
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Number : ");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+        bRet = nobj.CheckPrime(iValue);
+
+        if(bRet == true)
+        {
+            System.out.println("It is Prime ");
+        }
+        else
+        {
+            System.out.println("It is Not Prime");
+        }
+    }
+}
+// Time Complexity : O(N/2)
+// Where N >= 0
