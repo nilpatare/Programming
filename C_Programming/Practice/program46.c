a7a3feb46005957310078e377c4d8d8e824480e0
nilparate <nilpatare7700@gmail.com>
2026-07-14 22:02:26 +0530

    using do while loop

---
+++ C_Programming/Practice/program46.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1; 
+
+    do
+    {
+    printf("Jay Ganesh....\n");
+    iCnt++;
+    }while(iCnt <= 5);
+
+
+    return 0;
+}
+
+// change the while loop to do while loop of best simplicity
