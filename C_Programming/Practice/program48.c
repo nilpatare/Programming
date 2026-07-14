1e95d2fd4cfe000f81c03bbe9785fba02b6d25ba
nilparate <nilpatare7700@gmail.com>
2026-07-14 22:04:42 +0530

    using while loop print return

---
+++ C_Programming/Practice/program48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 5;
+
+    while( iCnt >= 1)
+    {
+    printf("%d\n",iCnt);
+    iCnt--;
+    }
+
+    printf("\n");
+    return 0;
+}
