b9e4d5f057a73865edfefd05d6abbd54328339e4
nilparate <nilpatare7700@gmail.com>
2026-07-16 23:58:40 +0530

    problem of n numbers

---
+++ Java_Programming/Practice/program37.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class DigitX
+{
+    public int CountDigit(int iNo)
+    {
+        int iCount = 0;
+
+        while(iNo != 0)
+        {
+            iCount++;
+            iNo = iNo / 10;
+        }
+        return iCount;
+    }
+}
+
+class program37
+{
+    public static void main (String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter Number");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.CountDigit(iValue);
+        
+        System.out.println("Number of Digit " + iRet);
+
+    }
+}
