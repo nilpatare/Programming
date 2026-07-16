fbd6ae095ce206b779d3659b01e22d1660393feb
nilparate <nilpatare7700@gmail.com>
2026-07-17 00:54:02 +0530

    sum count normal changes

---
+++ Java_Programming/Practice/program43.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class DigitX
+{
+    public int SumDigit(int iNo)
+    {
+        int iSum = 0;
+
+        while(iNo != 0)
+        {
+            iSum = iSum + (iNo % 10);
+            iNo = iNo / 10;
+        }
+        return iSum;
+    }
+}
+
+class program43
+{
+    public static void main (String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter Number");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.SumDigit(iValue);
+        
+        System.out.println("Summetion of the Number " + iRet);
+    }
+}
