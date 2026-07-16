3b7776f3ea7e0f7b3eb8aa9c4b48acef0f9c14b1
nilparate <nilpatare7700@gmail.com>
2026-07-17 00:52:59 +0530

    n number of sumDigit

---
+++ Java_Programming/Practice/program42.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class DigitX
+{
+    public int SumDigit(int iNo)
+    {
+        int iDigit = 0;
+        int iSum = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + iDigit;
+            iNo = iNo / 10;
+        }
+        return iSum;
+    }
+}
+
+class program42
+{
+    public static void main (String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter Number");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.SumDigit(iValue);
+        
+        System.out.println("Summetion of the Number " + iRet);
+    }
+}
