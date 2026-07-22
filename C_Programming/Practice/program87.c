3e64f919cd1b6fb7714ad463f866244351ccae2e
nilparate <nilpatare7700@gmail.com>
2026-07-22 19:08:55 +0530

    check element

---
+++ C_Programming/Practice/program87.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    bool bFlag = false;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+    return bFlag;
+}
+
+int main()
+{
+    int iLength = 0;
+    int iCnt = 0;
+    bool bRet = false;
+    int *Brr = NULL;
+
+    printf("Enter Number of Element : ");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the element : \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    bRet = LinearSearch(Brr, iLength);
+
+    if(bRet == true)
+    {
+        printf("Element is Present");
+    }
+    else
+    {
+        printf("Element is Not present");
+    }
+
+    free(Brr);
+
+    return 0;
+}
