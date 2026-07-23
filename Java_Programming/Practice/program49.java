57e685ed5274b7ff73fd54bde31031a1c78412ba
nilparate <nilpatare7700@gmail.com>
2026-07-23 23:23:56 +0530

    updater previous code

--- Java_Programming/Practice/program49.java
+++ Java_Programming/Practice/program49.java
@@ -1,19 +1,16 @@
-
 import java.util.Scanner;
 
-
-class program48
+class program49
 {
     public static void main(String A[])
     {
         Scanner sobj = new Scanner(System.in);
-        int iCnt = 0;
 
         int Arr[] = {10,20,30,40,50};
 
         System.out.println(Arr.length);
 
-        for(iCnt = 0; iCnt <Arr.length; iCnt++)
+        for(int iCnt = 0; iCnt <Arr.length; iCnt++)
         {
         System.out.println(Arr[iCnt]);
         }
