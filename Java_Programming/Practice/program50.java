85178a59dfe5953c70f08c3999eef3233a05de93
nilparate <nilpatare7700@gmail.com>
2026-07-23 23:24:43 +0530

    print the array error

---
+++ Java_Programming/Practice/program50.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class program139
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+
+        System.out.println(Arr.length);
+
+        // BAd Programming practice
+        for(int iCnt = 0; iCnt <Arr.length; iCnt++)
+        {
+        System.out.println(Arr[iCnt]);
+        }
+
+        System.out.println(iCnt);               // Error
+    }
+}
