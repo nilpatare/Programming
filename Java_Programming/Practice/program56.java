6de2ca21a9f5d9b989f5289819d75b84fbf0b949
nilparate <nilpatare7700@gmail.com>
2026-07-23 23:31:51 +0530

    using update the array

---
+++ Java_Programming/Practice/program56.java
@@ -0,0 +1,36 @@
+class ArrayX
+{
+public void Update(int Arr[], int iSize)
+    {
+        int iCnt = 0;
+        for(iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            Arr[iCnt]++;
+        }
+    }
+}
+
+class program147
+{
+   public static void main(String A[])
+    {
+        int Brr[] = {10,20,30,40,50};
+        int iCnt = 0;
+
+        System.out.println("Array element before call : ");
+        for(iCnt = 0 ; iCnt  < 5; iCnt++)
+        {
+            System.out.println(Brr[iCnt]);
+        }
+
+    ArrayX aobj = new ArrayX();
+    aobj.Update(Brr,5);
+
+    System.out.println("Array element After call ");
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        System.out.println(Brr[iCnt]);
+    }
+       
+    }
+}
