7df876ea2694ae8ce004443a1bd5e5780ce9181e
nilparate <nilpatare7700@gmail.com>
2026-07-25 21:55:49 +0530

    static memory allocation for object

---
+++ C++_Programming/Practice/program22.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        // Parameterise Constructor
+        ArrayX(int X)
+        {
+            cout<<"Inside Constructor"<<endl;
+            iSize = X;                       // Characteristics initialisation
+            Arr = new int[iSize];           // Resource allocation
+        }
+        // Destructor
+        ~ArrayX()
+        {
+            cout<<"Insdie destructor"<<endl;
+            delete[] Arr;                   // Resource Deallocatiohn
+        }
+};
+
+int main()
+{
+    // Static memory allocation for object
+    // ArrayX aobj1(5);   
+    
+    ArrayX *aobj1 = new ArrayX(5);
+
+    return 0;
+}
