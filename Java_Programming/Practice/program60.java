789bf7d3f9e3f90496e3e072b6137587ed84a6fa
nilparate <nilpatare7700@gmail.com>
2026-07-26 23:58:45 +0530

    pattern print

---
+++ Java_Programming/Practice/program60.java
@@ -0,0 +1,32 @@
+//Input : 5
+//Output : 1 * 2 *  3 *  4 *  5 * 
+
+import java.util.Scanner;
+
+class program60
+{
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.print(iCnt+"\t * \t" );
+        }
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+        
+
+        System.out.println("Enter the number of elements : ");
+ 
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+
+    }
+}
