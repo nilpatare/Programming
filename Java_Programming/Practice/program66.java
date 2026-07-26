8378ff840ce8ed4e12c1e6a95e9fae5cb999d961
nilparate <nilpatare7700@gmail.com>
2026-07-27 00:16:23 +0530

    pattern printing

---
+++ Java_Programming/Practice/program66.java
@@ -0,0 +1,34 @@
+//Input : 7
+//Output :   
+
+import java.util.Scanner;
+
+class program66
+{
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+        char ch = 'a';
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.print(ch+"\t");
+            ch++;
+        }
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+        
+
+        System.out.println("Enter the number of elements : ");
+ 
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+
+    }
+}
