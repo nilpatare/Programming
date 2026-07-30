4af31b937fd7020e9bf2dc1e1c3992a51012c81a
nilparate <nilpatare7700@gmail.com>
2026-07-30 22:33:49 +0530

    pattern print

--- Java_Programming/Practice/program77.java
+++ Java_Programming/Practice/program77.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-class program76
+class program77
 {
     public static void Display()
     {
@@ -17,6 +17,11 @@ class program76
        System.out.println("");
 
        for(iCnt = 1; iCnt <= 4; iCnt++)
+       {
+        System.out.print("*\t");
+       }
+
+    for(iCnt = 1; iCnt <= 4; iCnt++)
        {
         System.out.print("*\t");
        }
