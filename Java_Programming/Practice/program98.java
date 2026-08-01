4078f7f75e118bc84fe9d90cc1c48434cde0d402
nilparate <nilpatare7700@gmail.com>
2026-08-01 19:27:56 +0530

    pattern print

---
+++ Java_Programming/Practice/program98.java
@@ -0,0 +1,64 @@
+/*
+        iRow = 4
+        iCOl = 4
+
+       $    @   @   @
+       @    $   @   @
+       @    @   $   @
+       @    @   @   $
+*/
+
+
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0 , j = 0;
+
+        // filter for diagonal pattern 
+        if(iRow != iCol)
+            {
+                System.out.println("Invalid Parameters");
+                System.out.println("Number of rows and columns should be same");
+                return ;
+            }
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol ; j++)
+            {
+                if(i == j)
+                {
+                    System.out.print("$\t");
+                }
+                else
+                {
+                    System.out.print("@\t");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
+
+class program98
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Column : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
