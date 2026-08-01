0629c7308bf275080f00c82ee8d1847c11e310da
nilparate <nilpatare7700@gmail.com>
2026-08-01 19:25:21 +0530

    pattern print

---
+++ Java_Programming/Practice/program95.java
@@ -0,0 +1,49 @@
+/*
+        iRow = 4
+        iCOl = 4
+
+       4    4   4   4
+       3    3   3   3
+       2    2   2   2
+       1    1   1   1
+*/
+
+
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0 , j = 0;
+
+        for(i = iRow; i >= 1; i--)
+        {
+            for(j = 1; j <= iCol ; j++)
+            {
+              System.out.print(i + "\t");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+class program208
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Column : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
