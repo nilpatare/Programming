a4af8a06a68faea868f94ee276e8267b4e0283c8
nilparate <nilpatare7700@gmail.com>
2026-08-17 22:05:09 +0530

    sum non factor

---
+++ C_Programming/Competitive/program19.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int SumNonFact(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if(iNo % iCnt != 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+
+}
+
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    iRet = SumNonFact(iValue);
+
+    printf("Addition is %d\n",iRet);
+
+    return 0;
+}
