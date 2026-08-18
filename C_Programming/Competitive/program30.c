e98ba035345494e90251763a39bfff37a4bbce9a
nilparate <nilpatare7700@gmail.com>
2026-08-18 22:04:12 +0530

    print reverse 10 to 1

---
+++ C_Programming/Competitive/program30.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Table(int iNo)
+{
+    int iCnt = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    for(iCnt = 10; iCnt > 1; iCnt--)
+    {
+        printf("%d\t",iCnt * iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter A Number : ");
+    scanf("%d",&iValue);
+
+    Table(iValue);
+    return 0;
+}
