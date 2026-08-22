f9d149b291eb8e66ab4c0d16ed07311cfb65d3f0
nilparate <nilpatare7700@gmail.com>
2026-08-22 22:47:48 +0530

    Range Display

---
+++ C_Programming/Competitive/program51.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void RangeDisplay(int iStart, int iEnd)
+{
+    int iCnt = 0;
+
+    if(iStart > iEnd)
+    {
+        printf("Invalid range");
+        return;
+    }
+
+    for(iCnt = iStart; iCnt <= iEnd; iCnt++)
+    {
+        printf("%d\t", iCnt);
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter First Number : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter Second Number : ");
+    scanf("%d",&iValue2);
+
+    RangeDisplay(iValue1,iValue2);
+
+    return 0;
+}
