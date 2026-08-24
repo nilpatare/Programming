f962afa751b175e5b90785d5bd17e1fe2c33f075
nilparate <nilpatare7700@gmail.com>
2026-08-24 23:23:09 +0530

    check zero

---
+++ C_Programming/Competitive/program60.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int CheckZero(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit < 6)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    iRet = CheckZero(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
